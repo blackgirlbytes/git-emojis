9e387d33ae3afc3f1938a0cc9da77ba4f6c044f6
Rizel Scarlett <blackgirlbytes@github.com>
2021-09-16 21:33:48 -0400

    Initial setup (#4)
    * implemented nextjs, tailwind, and swr
    
    * search bar component
    
    * issue of function running before button click
    
    * the reason the onclick doesnt work
    
    * adding node modules to git ignore

---
+++ cutemoji/pages/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const getEmoji = (userInput, apiData) => {
+    // why is this running right away???? 
+    console.log('stop printing before I click the button')
+    console.log(apiData)
+    console.log(userInput)
+    // return an image with the user input from the api data so I could do something like data.octopus 
+    //and return the image for that
+}
+
+const SearchBar = ({ apiData }) => {
+    const [userInput, setUserInput] = useState('')
+    // console.log(userInput)
+
+    return (
+        <div class="p-8" >
+            <div class="bg-white flex items-center rounded-full shadow-xl">
+                <input class="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search" onChange={event => setUserInput(event.target.value)} />
+                <div class="p-4">
+                    <button type="submit" class="bg-red-400 text-white rounded-full p-2 hover:bg-red-300 focus:outline-none w-12 h-12 flex items-center justify-center" onClick={() => getEmoji(userInput, apiData)}>
+                        +
+                    </button>
+                </div>
+
+            </div>
+        </div>
+
+    );
+}
+
+export default SearchBar;
