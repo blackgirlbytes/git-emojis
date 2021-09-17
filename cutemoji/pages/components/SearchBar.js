a01dc3e56a0aa7e4e0211b254941da780f4a5ffb
Rizel Scarlett <blackgirlbytes@github.com>
2021-09-17 01:20:02 +0000

    the reason the onclick doesnt work

--- cutemoji/pages/components/SearchBar.js
+++ cutemoji/pages/components/SearchBar.js
@@ -18,7 +18,7 @@ const SearchBar = ({ apiData }) => {
             <div class="bg-white flex items-center rounded-full shadow-xl">
                 <input class="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search" onChange={event => setUserInput(event.target.value)} />
                 <div class="p-4">
-                    <button type="submit" class="bg-red-400 text-white rounded-full p-2 hover:bg-red-300 focus:outline-none w-12 h-12 flex items-center justify-center" onClick={getEmoji(userInput, apiData)}>
+                    <button type="submit" class="bg-red-400 text-white rounded-full p-2 hover:bg-red-300 focus:outline-none w-12 h-12 flex items-center justify-center" onClick={() => getEmoji(userInput, apiData)}>
                         +
                     </button>
                 </div>
