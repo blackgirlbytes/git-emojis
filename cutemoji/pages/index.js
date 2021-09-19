987d3b4b1980ff9ba72e7f0ac2ad417dc6876aa4
Rizel Scarlett <blackgirlbytes@github.com>
2021-09-19 16:21:34 +0000

    initial attempt at formatting data

--- cutemoji/pages/index.js
+++ cutemoji/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SearchBar from './components/SearchBar'
 import useSWR from "swr";
