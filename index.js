5815aab5fa2702bd555d09cfc4e088a3262a05be
Rizel <rizel@Rizels-MacBook-Pro.local>
2021-09-29 17:24:31 -0400

    uploading to an S3 bucket

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ const awsx = require("@pulumi/awsx");
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("git-emojis");
 
+
 // Export the name of the bucket
 exports.bucketName = bucket.id;
+
+const bucketObject = new aws.s3.BucketObject("cutemoji/pages/index.js", {
+    bucket: bucket,
+    source: new pulumi.asset.FileAsset("cutemoji/pages/index.js")
+});
