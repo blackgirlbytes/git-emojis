445d4333a894fea45c78e7f2da0a77ee1c37d1ff
Rizel <rizel@Rizels-MacBook-Pro.local>
2021-09-29 17:44:24 -0400

    add to a lambda

--- index.js
+++ index.js
@@ -5,19 +5,46 @@ const awsx = require("@pulumi/awsx");
 
 // Create an AWS resource (S3 Bucket)
 // const bucket = new aws.s3.Bucket("git-emojis");
-const bucket = new aws.s3.Bucket("git-emojis", {
-    website: {
-        indexDocument: "cutemoji/pages/index.js",
-    },
-});
-// Export the name of the bucket
-exports.bucketName = bucket.id;
+// const bucket = new aws.s3.Bucket("git-emojis", {
+//     website: {
+//         indexDocument: "./cutemoji/pages/index.js",
+//     },
+// });
+// // Export the name of the bucket
+// exports.bucketName = bucket.id;
 
-const bucketObject = new aws.s3.BucketObject("cutemoji/pages/index.js", {
-    bucket: bucket,
-    acl: "public-read",
-    contentType: "text/javascript",
-    source: new pulumi.asset.FileAsset("cutemoji/pages/index.js")
+// const bucketObject = new aws.s3.BucketObject("./cutemoji/pages/index.js", {
+//     bucket: bucket,
+//     acl: "public-read",
+//     contentType: "text/javascript",
+//     source: new pulumi.asset.FileAsset("./cutemoji/pages/index.js")
+// });
+
+// exports.bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
+
+const endpoint = new awsx.apigateway.API("hello", {
+    routes: [
+        // Serve static files from the `www` folder (using AWS S3)
+        {
+            path: "/",
+            localPath: "cutemoji",
+        },
+
+        // Serve a simple REST API on `GET /name` (using AWS Lambda)
+        {
+            path: "/source",
+            method: "GET",
+            eventHandler: (req, ctx, cb) => {
+                cb(undefined, {
+                    statusCode: 200,
+                    body: Buffer.from(JSON.stringify({ name: "AWS" }), "utf8").toString("base64"),
+                    isBase64Encoded: true,
+                    headers: { "content-type": "application/json" },
+                })
+            }
+        }
+    ]
 });
 
-exports.bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
+// Export the public URL for the HTTP service
+exports.url = endpoint.url;
