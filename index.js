7d07fce2d27ec5c8b073815464522aadb2b0b899
Rizel <rizel@Rizels-MacBook-Pro.local>
2021-09-29 17:34:24 -0400

    adding some more lines to host s3 bucket

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const aws = require("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("git-emojis");
-
-
+// const bucket = new aws.s3.Bucket("git-emojis");
+const bucket = new aws.s3.Bucket("git-emojis", {
+    website: {
+        indexDocument: "cutemoji/pages/index.js",
+    },
+});
 // Export the name of the bucket
 exports.bucketName = bucket.id;
 
 const bucketObject = new aws.s3.BucketObject("cutemoji/pages/index.js", {
     bucket: bucket,
+    acl: "public-read",
+    contentType: "text/javascript",
     source: new pulumi.asset.FileAsset("cutemoji/pages/index.js")
 });
+
+exports.bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
