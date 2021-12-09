3deccd8fe606740abd4768ad698001c0251702a0
blackgirlbytes <blackgirlbytes@github.com>
2021-12-09 02:34:52 +0000

    Deploying to gh-pages from @ blackgirlbytes/git-emojis@542b0c501746608cd7a746e337fb1ee4e615c0b7 🚀

---
+++ _next/static/TsnKkZ-NagYx9YcQ41lyQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/565-e91bba9bd71d0a71dfb8.js","static/css/eeb0a5bed78fdd918d4c.css","static/chunks/pages/index-4cc65188f27622c54bb0.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
